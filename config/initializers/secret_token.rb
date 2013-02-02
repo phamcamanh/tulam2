89a491a122cdc8b141731cfb6035badc61d9120e
phamcamanh <pham.cam.anh@framgia.com>
2013-02-02 10:14:00 +0700

    khoi tao

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tulam2::Application.config.secret_token = '0a2770948cec35b5fe3605c6b49010db065c35b5f84995c9d7921f937cd7a80cf98773c85893e7e9d58008f993598d4a32c81251d64f56f7f5afb8d3a36e78fb'
